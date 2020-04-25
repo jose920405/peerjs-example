80e6d407f0345da214c3212768e94f6925a76d7a
jose <jgonzalez@vovsolutions.com>
2020-04-25 15:30:54 -0500

    Example added

--- README.md
+++ README.md
@@ -1,68 +1,23 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Pasos para correr ejemplo.
 
-## Available Scripts
+Ejecutar los siguientes comandos
+```bash
+nvm use
+yarn install
+```
 
-In the project directory, you can run:
+abrir 2 tabs en la terminal. La idea es que en un tab simulemos al doctor y en el otro tab al paciente.
 
-### `yarn start`
+En el primer tab correr 
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+```bash
+REACT_APP_ROL=doctor yarn start
+```
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+En el segundo tab correr
 
-### `yarn test`
+```bash
+REACT_APP_ROL=paciente yarn start
+```
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+Si sale el siguiente mensaje `Would you like to run the app on another port instead?` escribir `y` y presionar `enter`.
--- src/App.js
+++ src/App.js
@@ -1,26 +1,177 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Peer from 'peerjs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let peer = null;
+let conn = null;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      joinText: null,
+      lastPeerId: null,
+    };
+
+    this.sendMessage = this.sendMessage.bind(this);
+    this.connectPatientToPeer = this.connectPatientToPeer.bind(this);
+  }
+
+  componentDidMount() {
+    peer = new Peer(null, { debug: 2 });
+    this.peerListeners();
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  componentDidUpdate() {
+
+  }
+
+  //#region Actions
+  sendMessage() {
+    if (!conn) {
+      return alert('Todavía no se ha establecido una conexión');
+    }
+
+    const isDoctor = this.isDoctor();
+    conn.send(`Mensaje de ${isDoctor ? 'doctor' : 'paciente'}`);
+  }
+  //#endregion Actions
+
+  //#region functions
+  isDoctor() {
+    return process.env.REACT_APP_ROL === 'doctor';
+  }
+
+  peerListeners() {
+    peer.on('connection', (connListener) => {
+      conn = connListener;
+      this.connectionListeners();
+    });
+
+    peer.on('open', (id) => {
+      console.log('My peer ID is: ' + id);
+      let lastPeerId = this.state.lastPeerId;
+
+      // Workaround for peer.reconnect deleting previous id
+      if (peer.id === null) {
+        console.log('Received null id from peer open');
+        peer.id = lastPeerId;
+      } else {
+        lastPeerId = peer.id;
+      }
+
+      this.setState({ lastPeerId });
+    });
+
+    peer.on('disconnected', function () {
+      console.log('Connection lost. Please reconnect');
+
+      // Workaround for peer.reconnect deleting previous id
+      // peer.id = lastPeerId; // Commented because cause an error releated with is only getter value
+      peer._lastServerId = this.state.lastPeerId;
+      peer.reconnect();
+    });
+
+    peer.on('error', (err) => {
+      console.log('30 peer on err in doctor >>> ', err);
+    });
+  }
+
+  connectionListeners() {
+    const isDoctor = this.isDoctor();
+    conn.on('open', () => {
+      if (!isDoctor) {
+        console.log('************ !!!PACIENTE CONECTADO AL DOCTOR!!!! *********** ');
+        conn.send('Hello!');
+      }
+    });
+
+    // Receive messages
+    conn.on('data', (data) => {
+      if (isDoctor) {
+        console.log('84 Mensaje recibido del paciente >>>>>>>>> ');
+      } else {
+        console.log('84 Mensaje recibido del doctor >>>>>>>>> ');
+      }
+
+      console.log('88 data >>> ', data);
+    });
+
+    conn.on('close', () => {
+      console.log('96 HERE CLOSE CONNECTION >>>>>>>>> ');
+      if (isDoctor) {
+        console.log('Doctor Closed ');
+      } else {
+        console.log('Patient Closed ');
+      }
+    });
+
+    conn.on('error', (err) => {
+      console.log('100 HERE ERROR >>>>>>>>> ', err);
+      if (isDoctor) {
+        console.log('Doctor error ');
+      } else {
+        console.log('Patient error ');
+      }
+      alert(err);
+    });
+  }
+
+  connectPatientToPeer() {
+    if (!this.state.joinText) {
+      alert('Por favor digite el id de la conexión generado desde el lado del doctor');
+      return;
+    }
+
+    conn = peer.connect(this.state.joinText, { label: 'Patient Connection', reliable: true });
+    this.connectionListeners();
+  }
+
+  //#endregion functions
+
+  //#region Render Functions
+  buildPatientConnectionForm() {
+    if (this.isDoctor()) {
+      return null;
+    }
+
+    return (
+      <div style={{ margin: '20px' }}>
+        <input type="text" id="receiver-id" title="Peer ID desde el paciente" onChange={(e) => { this.setState({ joinText: e.target.value }); }} />
+        <button id="connect-button" onClick={this.connectPatientToPeer}>{`Connectarse al doctor`}</button>
+      </div>
+    );
+  }
+
+  buildSessionIdLabel() {
+    if (!this.isDoctor() || !this.state.lastPeerId) {
+      return null;
+    }
+
+    return (
+      <div style={{ margin: '20px' }}>
+        <label>{`Copiar en el paciente:`}</label>
+        <label>{`ID: ${this.state.lastPeerId}`}</label>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div style={{ width: '100%' }}>
+        {this.buildSessionIdLabel()}
+        {this.buildPatientConnectionForm()}
+        <button onClick={this.sendMessage}>{`Enviar mensaje de prueba`}</button>
+      </div>
+    );
+  }
+  //#endregion Render Functions
 }
 
 export default App;
+
+//export default App;
